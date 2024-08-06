84f304bf756064874e9995fb8bcdd104573a45fb
Ajay Sonkar <114142664+ajaysonkar80@users.noreply.github.com>
2024-08-06 22:00:28 +0530

    Update http.go

--- http.go
+++ http.go
@@ -1,32 +1,39 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
+    "html/template"
+    "net/http"
+    "os"
 )
 
 type Todo struct {
-	Title string
-	Done  bool
+    Title string
+    Done  bool
 }
 
 type TodoPageData struct {
-	PageTitle string
-	Todos     []Todo
+    PageTitle string
+    Todos     []Todo
 }
 
 func main() {
-	tmpl := template.Must(template.ParseFiles("layout.html"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := TodoPageData{
-			PageTitle: "My TODO list",
-			Todos: []Todo{
-				{Title: "Task 1", Done: false},
-				{Title: "Task 2", Done: true},
-				{Title: "Task 3", Done: true},
-			},
-		}
-		tmpl.Execute(w, data)
-	})
-	http.ListenAndServe(":80", nil)
+    tmpl := template.Must(template.ParseFiles("layout.html"))
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        data := TodoPageData{
+            PageTitle: "My TODO list",
+            Todos: []Todo{
+                {Title: "Task 1", Done: false},
+                {Title: "Task 2", Done: true},
+                {Title: "Task 3", Done: true},
+            },
+        }
+        tmpl.Execute(w, data)
+    })
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080" // Default to port 8080 if not set
+    }
+
+    http.ListenAndServe(":" + port, nil)
 }
